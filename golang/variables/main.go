aef7c22676e8fa04c8f2edbb012f46631416300e
ivreone <ivreone@gmail.com>
2020-11-05 21:29:50 +0200

    add visible

--- golang/variables/main.go
+++ golang/variables/main.go
@@ -3,21 +3,23 @@ package main
 import "fmt"
 
 var y string = "visible"
+
 // x := "visible" // short entry is not available outside the function
 
 func main() {
-//    fmt.Println(x)
-    var x string
-    x = "first"
-    fmt.Println(x)
-    x += " second"
-    fmt.Println(x)
-//short write
-   myName := "Max"
-   fmt.Println("My name is", myName, "Hello")
+	//    fmt.Println(x)
+	var x string
+	x = "first"
+	fmt.Println(x)
+	x += " second"
+	fmt.Println(x)
+	//short write
+	myName := "Max"
+	fmt.Println("My name is", myName, "Hello")
+	fmt.Println(y)
 }
 
 func f() {
-//	fmt.Println(x)
+	//	fmt.Println(x)
 	fmt.Println(y)
 }
